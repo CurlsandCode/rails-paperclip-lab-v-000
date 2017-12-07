4bb241c62b4979ecb50d2766db7ed2c52e9d45ae
Monika Williams <monika.m.williams@gmail.com>
2017-12-07 11:59:20 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -9,4 +9,7 @@ class Song < ActiveRecord::Base
     artist = Artist.find_or_create_by(name: name)
     self.artist = artist
   end
+
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+ validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
